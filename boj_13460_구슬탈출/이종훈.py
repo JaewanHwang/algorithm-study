960503deb3920f07a0c16f94ba2ad23cbbc2ee8a
yijonghoon <yijonghoon@naver.com>
2023-01-28 20:33:00 +0900

    almost done

--- boj_13460_구슬탈출/이종훈.py
+++ boj_13460_구슬탈출/이종훈.py
@@ -1,42 +1,45 @@
 import sys
-sys.stedin = open('jh_input.txt')
 
-obstacle = ['#','R','B']
+sys.stdin = open('jh_input.txt')
+
+obstacle = ['#', 'R', 'B']
+
+
 def rollingup(location):
     return location
-def rollingdown(location):
+
+
+def rollingdown(location, signal):
     stop = False
     rstop = False
     bstop = False
     rgoal = False
     bgoal = False
-    goal = False
 
-    while stop == False:
-        for i in range(N):
+    while not stop:
+        for i in range(N - 1, -1, -1):
             for j in range(M):
                 if location[i][j] == 'R':
-                    if location[i+1][j] in obstacle:
+                    if location[i + 1][j] in obstacle:
                         rstop = True
-                    elif location[i+1][j] == 'B':
+                    elif location[i + 1][j] == 'B':
                         rstop = True
-                    elif location[i+1][j] == '0':
+                    elif location[i + 1][j] == '0':
                         rgoal = True
                         location[i][j] == '.'
-                        location[i + 1][j] = '1' #0을 지나가고 있는 R인 경우 1로 표시
+                        location[i + 1][j] = '1'  # 0을 지나가고 있는 R인 경우 1로 표시
                     else:
                         location[i][j] == '.'
                         location[i + 1][j] = 'R'
 
                 if location[i][j] == '1':
-                    if location[i+1][j] in obstacle:
+                    if location[i + 1][j] in obstacle:
                         rstop = True
-                    elif location[i+1][j] == 'B':
+                    elif location[i + 1][j] == 'B':
                         rstop = True
                     else:
                         location[i][j] == '0'
                         location[i + 1][j] = 'R'
-
 
                 if location[i][j] == 'B':
                     if location[i + 1][j] in obstacle:
@@ -61,52 +64,87 @@ def rollingdown(location):
                         location[i + 1][j] = 'B'
                 if rstop and bstop == True:
                     stop = True
-                    stop = True
-    if bgoal == True:
-        return
+    if bgoal == True:  # 파란공이 goal에 들어가면 signal은 2
+        signal = 2
+    elif rgoal == True:  # 파란공이 들어가지 않고 빨간공이 들어가면 signal은 1
+        signal = 1
+
+    return location, signal
 
 
-    return location
 def rollingleft(location):
     return location
+
+
 def rollingright(location):
     return location
 
-def beedescape(progress,direction,count):
-    if direction == 'up':
-        progress = rollingdown(progress)
-        beedescape(progress,'up',count + 1)
-        beedescape(progress, 'down', count + 1)
-        beedescape(progress, 'left', count + 1)
-        beedescape(progress, 'right', count + 1)
-    elif direction == 'down':
-        progress = rollingdown(progress)
-        beedescape(progress,'up',count + 1)
-        beedescape(progress, 'down', count + 1)
-        beedescape(progress, 'left', count + 1)
-        beedescape(progress, 'right', count + 1)
-    elif direction == 'left':
-        progress = rollingdown(progress)
-        beedescape(progress,'up',count + 1)
-        beedescape(progress, 'down', count + 1)
-        beedescape(progress, 'left', count + 1)
-        beedescape(progress, 'right', count + 1)
 
-    elif direction == 'right':
-        progress = rollingdown(progress)
-        beedescape(progress,'up',count + 1)
-        beedescape(progress, 'down', count + 1)
-        beedescape(progress, 'left', count + 1)
-        beedescape(progress, 'right', count + 1)
+def beedescape(progress, direction, count):
+    global top
+    global answer
+    if count <= top:
+        if direction == 'up':
+            status = 0  # status가 0이면 킵고잉 status가 1이면 성공 status가 2면 스톱
+            progress, status = rollingup(progress, status)
+            if status == 0:
+                beedescape(progress, 'up', count + 1)
+                beedescape(progress, 'down', count + 1)
+                beedescape(progress, 'left', count + 1)
+                beedescape(progress, 'right', count + 1)
+            elif status == 1:
+                if count <= top:
+                    top = count
+                    answer = count
+
+        elif direction == 'down':
+            status = 0
+            progress, status = rollingdown(progress, status)
+            if status == 0:
+                beedescape(progress, 'up', count + 1)
+                beedescape(progress, 'down', count + 1)
+                beedescape(progress, 'left', count + 1)
+                beedescape(progress, 'right', count + 1)
+            elif status == 1:
+                if count <= top:
+                    top = count
+                    answer = count
+        elif direction == 'left':
+            status = 0
+            progress = rollingleft(progress, status)
+            if status == 0:
+                beedescape(progress, 'up', count + 1)
+                beedescape(progress, 'down', count + 1)
+                beedescape(progress, 'left', count + 1)
+                beedescape(progress, 'right', count + 1)
+            elif status == 1:
+                if count <= top:
+                    top = count
+                    answer = count
+
+        elif direction == 'right':
+            status = 0
+            progress = rollingright(progress, status)
+            if status == 0:
+                beedescape(progress, 'up', count + 1)
+                beedescape(progress, 'down', count + 1)
+                beedescape(progress, 'left', count + 1)
+                beedescape(progress, 'right', count + 1)
+            elif status == 1:
+                if count <= top:
+                    top = count
+                    answer = count
+
 
 answer = -1
 top = 10
-N, M = map(int,input().split())
+N, M = map(int, input().split())
 board = []
 for _ in range(N):
     board += [list(input())]
 print(board)
-beedescape(board,'up',1)
-beedescape(board,'down',1)
-beedescape(board,'left',1)
-beedescape(board,'right',1)
+beedescape(board, 'up', 1)
+beedescape(board, 'down', 1)
+beedescape(board, 'left', 1)
+beedescape(board, 'right', 1)
+print(answer)
