a3d53c561842e2d2fa8dce73c952dcb75b9df8ac
Jaewan Hwang <jaewan9074@gmail.com>
2023-02-12 12:34:40 +0900

    2023.02.12_경주로 건설 2

--- pro_경주로 건설/황재완.py
+++ pro_경주로 건설/황재완.py
@@ -1,3 +1,4 @@
+# 풀이1 dfs
 dx, dy = (0, 1, 0, -1), (1, 0, -1, 0)
 ans, cost, N, m = float('inf'), None, None, None
 
@@ -23,4 +24,31 @@ def solution(board):
     cost[0][0] = 0
     go(0, 0, 0)
     go(0, 0, 1)
+    return ans
+
+#----------------------------------------------------
+
+# 풀이2 bfs
+from collections import deque
+
+dx, dy = (0, 1, 0, -1), (1, 0, -1, 0)
+
+
+def solution(board):
+    m = board
+    N = len(m)
+    cost = [[[-1] * 4 for _ in range(N)] for _ in range(N)]
+    for d in range(4):
+        cost[0][0][d] = 0
+    q = deque([[0, 0, 0], [0, 0, 1]])
+    while q:
+        x, y, d = q.popleft()
+        for nd in range(4):
+            nx, ny = x + dx[nd], y + dy[nd]
+            new_cost = cost[x][y][d] + (100 if d == nd else 600)
+            if 0 <= nx < N and 0 <= ny < N and m[nx][ny] == 0 and (
+                    cost[nx][ny][nd] == -1 or cost[nx][ny][nd] > new_cost):
+                cost[nx][ny][nd] = new_cost
+                q.append((nx, ny, nd))
+    ans = min(filter(lambda x: x >= 0, cost[N - 1][N - 1]))
     return ans
