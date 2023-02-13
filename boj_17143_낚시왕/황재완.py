da2471a890bf1fc9cd34232c81e79f114eaa4e62
Jaewan Hwang <jaewan9074@gmail.com>
2023-02-13 10:41:36 +0900

    2023.02.13_낚시왕 2

--- boj_17143_낚시왕/황재완.py
+++ boj_17143_낚시왕/황재완.py
@@ -1,3 +1,4 @@
+# 풀이1
 import sys
 
 sys.stdin = open('input.txt')
@@ -44,4 +45,55 @@ for c in range(C):
                 continue
             move(x, y)
     m = tm
+print(ans)
+
+# 풀이2
+import sys
+
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+dx, dy = (-1, 1, 0, 0), (0, 0, 1, -1)
+
+R, C, M = map(int, input().split())
+m = [[-1] * C for _ in range(R)]
+shark_list = dict()
+for shark_num in range(M):
+    r, c, s, d, z = map(int, input().split())
+    m[r - 1][c - 1] = shark_num
+    shark_list[shark_num] = [s, d - 1, z, r - 1, c - 1]
+
+
+def move(shark_num):
+    s, d, z, x, y = shark_list[shark_num]
+    period = 2 * (R if 0 <= d <= 1 else C) - 2
+    for _ in range(s % period):
+        if x + dx[d] < 0 or x + dx[d] >= R or y + dy[d] < 0 or y + dy[d] >= C:
+            d = 1 - d if 0 <= d <= 1 else 5 - d
+        x, y = x + dx[d], y + dy[d]
+
+    if tm[x][y] == -1:
+        tm[x][y] = shark_num
+        shark_list[shark_num] = [s, d, z, x, y]
+    else:
+        if shark_list[tm[x][y]][2] < z:
+            shark_list[shark_num] = [s, d, z, x, y]
+            shark_list.pop(tm[x][y])
+            tm[x][y] = shark_num
+        else:
+            shark_list.pop(shark_num)
+
+
+ans = 0
+for c in range(C):
+    for r in range(R):
+        if m[r][c] != -1:
+            ans += shark_list[m[r][c]][2]
+            shark_list.pop(m[r][c])
+            break
+    # 상어의 이동
+    tm = [[-1] * C for _ in range(R)]
+    for shark_num in list(shark_list.keys()):
+        move(shark_num)
+    m = tm
+
 print(ans)
