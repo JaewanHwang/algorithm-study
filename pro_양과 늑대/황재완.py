a538626a85d6bea17bd0cad99b7d0200cdde54aa
Jaewan Hwang <jaewan9074@gmail.com>
2023-02-20 15:41:48 +0900

    2023.02.20_양과 늑대 2

--- pro_양과 늑대/황재완.py
+++ pro_양과 늑대/황재완.py
@@ -1,3 +1,4 @@
+# 풀이1: BFS with bitmasking
 import heapq
 
 ans = 0
@@ -76,4 +77,38 @@ def solution(info, edges):
                 if d[n][s][w]:
                     ans = max(ans, s)
 
+    return ans
+
+
+# 풀이 2: 상태를 기록하는 DFS
+from collections import deque
+
+ans = 0
+binary_tree = None
+info = None
+
+
+def go(n, sheep_cnt, wolf_cnt, edge_set):
+    global ans
+    cur_edge_set = set(edge_set)
+    cur_edge_set.remove(n)
+    sheep_cnt += info[n] ^ 1
+    wolf_cnt += info[n]
+    if wolf_cnt >= sheep_cnt:
+        return
+    ans = max(ans, sheep_cnt)
+    for v in binary_tree[n]:
+        cur_edge_set.add(v)
+    for v in cur_edge_set:
+        go(v, sheep_cnt, wolf_cnt, cur_edge_set)
+
+
+def solution(nodes, edges):
+    global info, binary_tree
+    info = nodes
+    N = len(info)
+    binary_tree = [[] for _ in range(N)]
+    for u, v in edges:
+        binary_tree[u].append(v)
+    go(0, 0, 0, {0})
     return ans
