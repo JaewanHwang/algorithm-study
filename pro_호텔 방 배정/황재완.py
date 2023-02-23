1e05117c54444ca2858372fd36e63b51d238800e
Jaewan Hwang <jaewan9074@gmail.com>
2023-02-23 18:01:28 +0900

    2023.02.23_호텔 방 배정

--- pro_호텔 방 배정/황재완.py
+++ pro_호텔 방 배정/황재완.py
@@ -1,15 +1,22 @@
-import bisect
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+
+
+def go(num, room_dict):
+    if num not in room_dict:
+        room_dict[num] = num + 1
+        return num
+    room_dict[num] = go(room_dict[num], room_dict)
+    return room_dict[num]
 
 
 def solution(k, room_number):
+    ans = [0] * len(room_number)
     room_dict = dict()
-    ans = []
-    for request_num in room_number:
-        if request_num not in room_dict:
-            room_dict[request_num] = request_num + 1
-        else:
-            while request_num in room_dict:
-                request_num = room_dict[request_num]
-            room_dict[request_num] = request_num + 1
-        ans.append(request_num)
+    for i, num in enumerate(room_number):
+        ans[i] = go(num, room_dict)
     return ans
+
+
+print(solution(10, [1, 3, 4, 1, 3, 1]))
