152deba9d1898f2c249445a96546baad5e83b423
Jaewan Hwang <jaewan9074@gmail.com>
2023-02-27 15:00:25 +0900

    2023.02.27_자동완성 3

--- pro_자동완성/황재완.py
+++ pro_자동완성/황재완.py
@@ -1,3 +1,4 @@
+# 풀이1
 import sys
 
 sys.setrecursionlimit(10 ** 6)
@@ -32,6 +33,37 @@ def solution(words):
         cur.word = word
 
     go(trie)
+
+    ans = 0
+    for word in words:
+        cur = trie
+        for cnt, char in enumerate(word, start=1):
+            cur = cur.children[char]
+            if cur.promising == 1:
+                break
+        ans += cnt
+
+    return ans
+
+
+# 풀이2
+class Node:
+    def __init__(self):
+        self.promising = 0
+        self.word = None
+        self.children = dict()
+
+
+def solution(words):
+    trie = Node()  # [단어 존재 여부, children{}]
+    for word in words:
+        cur = trie
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = Node()
+            cur = cur.children[char]
+            cur.promising += 1
+        cur.word = word
 
     ans = 0
     for word in words:
