81789e0e2c5b7b5642f044541ffe0ccb4c424f55
Jaewan Hwang <jaewan9074@gmail.com>
2023-02-27 14:55:55 +0900

    2023.02.27_자동완성 2

--- pro_자동완성/황재완.py
+++ pro_자동완성/황재완.py
@@ -7,42 +7,39 @@ class Node:
     def __init__(self):
         self.promising = 0
         self.word = None
-        self.childs = dict()
+        self.children = dict()
 
 
 def go(cur):
-    if cur.word != None:
+    if cur.word:
         cur.promising += 1
 
-    for child in cur.childs:
-        cur.promising += go(cur.childs[child])
+    for child in cur.children:
+        cur.promising += go(cur.children[child])
 
     return cur.promising
 
 
 def solution(words):
-    trie = Node()  # [단어 존재 여부, child{}]
+    trie = Node()  # [단어 존재 여부, children{}]
     for word in words:
         cur = trie
-        for i in range(len(word)):
-            char = word[i]
-            if char not in cur.childs:
-                child = Node()
-                cur.childs[char] = child
-            cur = cur.childs[char]
-            if i == len(word) - 1:
-                cur.word = word
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = Node()
+
+            cur = cur.children[char]
+        cur.word = word
+
     go(trie)
+
     ans = 0
     for word in words:
         cur = trie
-        for i in range(len(word)):
-            char = word[i]
-            cur = cur.childs[char]
+        for cnt, char in enumerate(word, start=1):
+            cur = cur.children[char]
             if cur.promising == 1:
-                ans += i + 1
                 break
-        else:
-            ans += i + 1
+        ans += cnt
 
     return ans
