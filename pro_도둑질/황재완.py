f94f2e73c60f2edd5b134b495e704147726d216c
Jaewan Hwang <jaewan9074@gmail.com>
2023-03-01 17:03:07 +0900

    2023.03.01_도둑질 2

--- pro_도둑질/황재완.py
+++ pro_도둑질/황재완.py
@@ -1,3 +1,5 @@
+# 풀이1 Top-down
+
 import sys
 
 sys.setrecursionlimit(10 ** 6)
@@ -22,4 +24,18 @@ def solution(money):
     return ans
 
 
-print(solution([1, 2, 3, 1]))
+# 풀이2 Bottom-up
+
+def solution(money):
+    N = len(money)
+    d = [[-1] * 2 for _ in range(N)]
+    d[0][0] = 0
+    d[0][1] = money[0]
+    d[1][1] = money[0]
+    d[1][0] = money[1]
+
+    for first in (0, 1):
+        for i in range(2, N - 1):
+            d[i][first] = max(d[i - 2][first] + money[i], d[i - 1][first])
+    ans = max(money[N - 1] + d[N - 3][0], d[N - 2][1])
+    return ans
