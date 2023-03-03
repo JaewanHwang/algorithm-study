528bcda91f829874952c1a6704122b89f265330d
Jaewan Hwang <jaewan9074@gmail.com>
2023-03-03 10:25:52 +0900

    2023.03.03_미로 탈출 명령어 3

--- pro_미로 탈출 명령어/황재완.py
+++ pro_미로 탈출 명령어/황재완.py
@@ -35,4 +35,39 @@ def solution(n, m, x, y, r, c, k):
 
     return ans[::-1]
 
+
 # 풀이 2: DFS + 그리디
+from collections import deque
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+dx, dy = (1, 0, 0, -1), (0, -1, 1, 0)
+dir_to_op = ['d', 'l', 'r', 'u']
+
+
+def go(x, y, d, k, board, ans, visited, n, m):
+    visited[x][y][d] = True
+    if board[x][y] == 'E' and d == k:
+        return True
+    if d == k:
+        return False
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny][d + 1]:
+            ans[d] = dir_to_op[i]
+            if go(nx, ny, d + 1, k, board, ans, visited, n, m):
+                return True
+    return False
+
+
+def solution(n, m, x, y, r, c, k):
+    sx, sy, ex, ey = x - 1, y - 1, r - 1, c - 1
+    visited = [[[False] * (k + 1) for _ in range(m)] for _ in range(n)]
+    board = [['.'] * m for _ in range(n)]
+    board[ex][ey], board[sx][sy] = 'E', 'S'
+    ans = [0] * k
+    go(sx, sy, 0, k, board, ans, visited, n, m)
+    if not visited[ex][ey][k]:
+        return 'impossible'
+    ans = ''.join(ans)
+    return ans
