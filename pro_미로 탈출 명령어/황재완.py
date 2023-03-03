d43ffba0bbfb900f921c1753e1f34f6f12d33928
Jaewan Hwang <jaewan9074@gmail.com>
2023-03-03 09:50:46 +0900

    2023.03.03_미로 탈출 명령어

---
+++ pro_미로 탈출 명령어/황재완.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+dx, dy = (1, 0, 0, -1), (0, -1, 1, 0)
+dir_to_op = ['d', 'l', 'r', 'u']
+
+
+def bfs(x, y, n, m, d, k):
+    d[x][y][0] = (-1, -1, -1)
+    q = deque([(x, y, 0)])
+    while q:
+        x, y, dist = q.popleft()
+        if dist == k:
+            continue
+        for dir in range(4):
+            nx, ny = x + dx[dir], y + dy[dir]
+            if 0 <= nx < n and 0 <= ny < m and d[nx][ny][dist + 1] == -1:
+                d[nx][ny][dist + 1] = (x, y, dir)
+                q.append((nx, ny, dist + 1))
+
+
+def solution(n, m, x, y, r, c, k):
+    sx, sy, ex, ey = x - 1, y - 1, r - 1, c - 1
+    d = [[[-1] * (k + 1) for _ in range(m)] for _ in range(n)]
+    bfs(sx, sy, n, m, d, k)
+
+    if d[ex][ey][k] == -1:
+        return 'impossible'
+    ans = ''
+    x, y, dist = ex, ey, k
+    while dist > 0:
+        x, y, dir = d[x][y][dist]
+        ans += dir_to_op[dir]
+        dist -= 1
+
+    return ans[::-1]
