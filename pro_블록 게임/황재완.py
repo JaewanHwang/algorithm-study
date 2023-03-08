e485c1f67382b5de366c1f2ef5b67de93f87668a
Jaewan Hwang <jaewan9074@gmail.com>
2023-03-08 14:14:48 +0900

    2023.03.08_블록 게임

--- pro_블록 게임/황재완.py
+++ pro_블록 게임/황재완.py
@@ -5,6 +5,15 @@ patterns = [[(1, 0), (1, 1), (1, 2), (0, 1), (0, 2)],
             [(1, -1), (1, 0), (1, 1), (0, -1), (0, 1)]]
 
 
+def ok(board, x, y, pi):
+    for dx, dy in patterns[pi][3:]:
+        nx, ny = x + dx, y + dy
+        for r in range(nx, -1, -1):
+            if board[r][ny] != 0:
+                return False
+    return True
+
+
 def solution(board):
     N = len(board)
     ans = 0
@@ -13,49 +22,28 @@ def solution(board):
         for y in range(N):
             if board[x][y] > 0:
                 for pi, pattern in enumerate(patterns):
-                    ok = True
+                    failed = False
                     for dx, dy in pattern[:3]:
                         if not (0 <= x + dx < N and 0 <= y + dy < N and board[x][y] == board[x + dx][y + dy]):
-                            ok = False
+                            failed = True
                             break
-                    if ok:
+                    if not failed:
                         candidate.add((x, y, pi))
                         break
     while True:
         remove_candidate = set()
         for x, y, pi in candidate:
-            remove_position = set([(x, y)])
-            ok = True
-            for dx, dy in patterns[pi][:3]:
-                nx, ny = x + dx, y + dy
-                remove_position.add((nx, ny))
-                for r in range(nx, -1, -1):
-                    if board[r][ny] != 0 and board[r][ny] != board[x][y]:
-                        ok = False
-                        break
-                if not ok:
-                    break
-            if ok:
-                for dx, dy in patterns[pi][3:]:
-                    nx, ny = x + dx, y + dy
-                    if board[nx][ny] > 0:
-                        ok = False
-                        break
-            if ok:
-                for rx, ry in remove_position:
-                    board[rx][ry] = 0
+            if ok(board, x, y, pi):
+                board[x][y] = 0
+                for dx, dy in patterns[pi][:3]:
+                    board[x + dx][y + dy] = 0
                 ans += 1
                 remove_candidate.add((x, y, pi))
 
         if not remove_candidate:
             break
 
-        candidate -= remove_candidate
+        for remove in remove_candidate:
+            candidate.remove(remove)
 
     return ans
-
-
-print(solution([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 4, 0, 0, 0], [0, 0, 0, 0, 0, 4, 4, 0, 0, 0],
-                [0, 0, 0, 0, 3, 0, 4, 0, 0, 0], [0, 0, 0, 2, 3, 0, 0, 0, 5, 5], [1, 2, 2, 2, 3, 3, 0, 0, 0, 5],
-                [1, 1, 1, 0, 0, 0, 0, 0, 0, 5]]))
