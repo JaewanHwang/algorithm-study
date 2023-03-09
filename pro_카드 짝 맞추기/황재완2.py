56ed8c8adbfc811068ee42989fb8e3cf8a9ddf37
Jaewan Hwang <jaewan9074@gmail.com>
2023-03-09 16:26:59 +0900

    2023.03.09_카드 짝 맞추기 2

--- pro_카드 짝 맞추기/황재완2.py
+++ pro_카드 짝 맞추기/황재완2.py
@@ -3,12 +3,15 @@ from collections import deque, defaultdict
 dx, dy = (-1, 1, 0, 0), (0, 0, -1, 1)
 
 
-def bfs(sx, sy):
+def bfs(sx, sy, ex, ey):
     d = [[-1] * 4 for _ in range(4)]
     q = deque([(sx, sy)])
     d[sx][sy] = 0
     while q:
         x, y = q.popleft()
+        if (x, y) == (ex, ey):
+            return d[ex][ey]
+
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
             if 0 <= nx < 4 and 0 <= ny < 4 and d[nx][ny] == -1:
@@ -23,8 +26,6 @@ def bfs(sx, sy):
                 d[nx][ny] = d[x][y] + 1
                 q.append((nx, ny))
 
-    return d
-
 
 def go(i, x, y, tot):
     global ans, cnt
@@ -37,14 +38,12 @@ def go(i, x, y, tot):
         if visited[card]:
             continue
         (x1, y1), (x2, y2) = cards[card]
-        between = bfs(x1, y1)[x2][y2] + 2
-        d = bfs(x, y)
-        dist1 = d[x2][y2]
-        dist2 = d[x1][y1]
+        dist1 = bfs(x, y, x1, y1) + bfs(x1, y1, x2, y2)
+        dist2 = bfs(x, y, x2, y2) + bfs(x2, y2, x1, y1)
         visited[card] = True
         m[x1][y1] = m[x2][y2] = 0
-        go(i + 1, x1, y1, tot + dist1 + between)
-        go(i + 1, x2, y2, tot + dist2 + between)
+        go(i + 1, x2, y2, 2 + tot + dist1)
+        go(i + 1, x1, y1, 2 + tot + dist2)
         m[x1][y1] = m[x2][y2] = card
         visited[card] = False
 
@@ -71,5 +70,4 @@ def solution(board, r, c):
     return ans
 
 
-print(solution([[1, 1, 2, 2], [3, 3, 4, 4], [5, 5, 6, 6], [0, 0, 0, 0]], 0, 0))
-print(cnt)
+print(solution([[3, 0, 0, 2], [0, 0, 1, 0], [0, 1, 0, 0], [2, 0, 0, 3]], 0, 1))
