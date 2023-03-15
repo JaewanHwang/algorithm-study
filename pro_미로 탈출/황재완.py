41f9d2786d6391734a74b7edd298ccec9e73a080
Jaewan Hwang <jaewan9074@gmail.com>
2023-03-15 12:12:35 +0900

    2023.03.15_미로 탈출 2

--- pro_미로 탈출/황재완.py
+++ pro_미로 탈출/황재완.py
@@ -1,3 +1,4 @@
+# 풀이 1: BFS
 from collections import deque
 
 
@@ -81,4 +82,60 @@ def solution(n, start, end, roads, traps):
     return ans
 
 
-print(solution(3, 1, 3, [[1, 2, 2], [3, 2, 3]], [2]))
+# 풀이 2: 다익스트라 with 상태
+import heapq
+
+
+def solution(n, start, end, roads, traps):
+    d = [[-1] * (2 ** len(traps)) for _ in range(n + 1)]
+    trap_map = dict()
+    traps = set(traps)
+    for i, trap in enumerate(traps):
+        trap_map[trap] = i
+    adj_mat = [[3000] * (n + 1) for _ in range(n + 1)]
+    out_graph = [set() for _ in range(n + 1)]
+    in_graph = [set() for _ in range(n + 1)]
+    for x, y, c in roads:
+        adj_mat[x][y] = min(adj_mat[x][y], c)
+        out_graph[x].add(y)
+        in_graph[y].add(x)
+
+    pq = [(0, start, 0)]
+
+    while pq:
+        dist, x, s = heapq.heappop(pq)
+        if d[x][s] != -1:
+            continue
+        d[x][s] = dist
+        for y in out_graph[x]:
+            if x not in traps and (y not in traps or (y in traps and not s & 1 << trap_map[y])):
+                ns = s | 1 << trap_map[y] if y in traps else s
+            elif x in traps and not s & 1 << trap_map[x] and (
+                    y not in traps or (y in traps and not s & 1 << trap_map[y])):
+                ns = s | 1 << trap_map[y] if y in traps else s
+            elif x in traps and s & 1 << trap_map[x] and y in traps and s & 1 << trap_map[y]:
+                ns = s ^ 1 << trap_map[y]
+            else:
+                continue
+            if d[y][ns] == -1:
+                heapq.heappush(pq, (d[x][s] + adj_mat[x][y], y, ns))
+        for y in in_graph[x]:
+            if x not in traps and y in traps and s & 1 << trap_map[y]:
+                ns = s ^ 1 << trap_map[y]
+            elif x in traps and s & 1 << trap_map[x] and (y not in traps or (y in traps and not s & 1 << trap_map[y])):
+                ns = s | 1 << trap_map[y] if y in traps else s
+            elif x in traps and not s & 1 << trap_map[x] and y in traps and s & 1 << trap_map[y]:
+                ns = s ^ 1 << trap_map[y]
+            else:
+                continue
+            if d[y][ns] == -1:
+                heapq.heappush(pq, (d[x][s] + adj_mat[y][x], y, ns))
+
+    ans = -1
+    for s in range(2 ** len(traps)):
+        if ans == -1 or 0 < d[end][s] < ans:
+            ans = d[end][s]
+    return ans
+
+
+print(solution(4, 1, 4, [[1, 2, 1], [3, 2, 1], [2, 4, 1]], [2, 3]))
