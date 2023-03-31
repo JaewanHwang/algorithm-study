23045f87e268b83bb0efb9bc3fbf48de3013a5b5
Jaewan Hwang <jaewan9074@gmail.com>
2023-03-31 10:00:51 +0900

    2023.03.31_다트 게임 2

--- pro_다트 게임/황재완_2c.py
+++ pro_다트 게임/황재완_2c.py
@@ -1,21 +1,22 @@
+import re
+
+
 def solution(dartResult):
     i = 0
     ans = []
     bonus_map = {'S': 1, 'D': 2, 'T': 3}
-    while i < len(dartResult):
-        if dartResult[i + 1].isdigit():
-            i += 1
-            cur = 10 ** bonus_map[dartResult[i + 1]]
-        else:
-            cur = int(dartResult[i]) ** bonus_map[dartResult[i + 1]]
-        i += 2
-        if i < len(dartResult) and not dartResult[i].isdigit():
-            if dartResult[i] == '*':
-                if len(ans) > 0:
+    for result in re.findall(r'\d+[SDT][*#]?', dartResult):
+        i = 1
+        if result[:2] == '10':
+            i = 2
+        cur = int(result[:i]) ** bonus_map[result[i]]
+        i += 1
+        if i == len(result) - 1:
+            if result[i] == '*':
+                if ans:
                     ans[-1] *= 2
                 cur *= 2
             else:
                 cur *= -1
-            i += 1
         ans.append(cur)
     return sum(ans)
