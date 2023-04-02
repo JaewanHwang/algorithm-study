e10e215d9e960c0553940ef0406a69773b57cdc2
Jaewan Hwang <jaewan9074@gmail.com>
2023-04-03 08:53:23 +0900

    2023.04.03_메뉴 리뉴얼_3c

--- pro_메뉴 리뉴얼/황재완_2c.py
+++ pro_메뉴 리뉴얼/황재완_2c.py
@@ -1,26 +1,37 @@
-from itertools import combinations
+def go(i, start, s, course, orders, all_menus):
+    if i in course:
+        cnt = 0
+        for order in orders:
+            if s <= order:
+                cnt += 1
+        if cnt >= 2:
+            candidates[i].append((cnt, ''.join(sorted(s))))
+        else:
+            return
+    for j in range(start, len(all_menus)):
+        s.add(all_menus[j])
+        go(i + 1, j + 1, s, course, orders, all_menus)
+        s.remove(all_menus[j])
+
+
+candidates = [[] for _ in range(11)]
 
 
 def solution(orders, course):
     all_menus = set()
     for i, order in enumerate(orders):
+        all_menus |= set(orders[i])
         orders[i] = set(order)
-        all_menus |= orders[i]
+    go(0, 0, set(), set(course), orders, sorted(all_menus))
     ans = []
     for l in course:
-        candidates = []
-        for case in combinations(all_menus, r=l):
-            cnt = 0
-            menu = set(case)
-            for order in orders:
-                if menu <= order:
-                    cnt += 1
-            if cnt >= 2:
-                candidates.append((cnt, ''.join(sorted(menu))))
-        candidates.sort(reverse=True)
-        for cnt, menu in candidates:
-            if cnt == candidates[0][0]:
-                ans.append(menu)
+        candidates[l].sort(reverse=True)
+        for cnt, s in candidates[l]:
+            if cnt == candidates[l][0][0]:
+                ans.append(s)
             else:
                 break
     return sorted(ans)
+
+
+print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4]))
