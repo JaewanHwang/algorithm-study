952acef121b74137336b8f6dc54d48a6d9b03349
Jaewan Hwang <jaewan9074@gmail.com>
2023-04-06 18:02:12 +0900

    2023.04.06_보호 필름_2c

--- swea_보호 필름/황재완_2c.py
+++ swea_보호 필름/황재완_2c.py
@@ -1,40 +1,40 @@
 import sys
-from itertools import combinations
 
 sys.stdin = open("input.txt", "r")
 
 
-def check():
+def check(x, max_cons, last_cons):
+    res = True
     for y in range(W):
-        last, cnt = -1, 0
-        ok = False
-        for x in range(D):
-            if m[x][y] == last:
-                cnt += 1
-            else:
-                cnt = 1
-                last = m[x][y]
-            if cnt >= K:
-                ok = True
-                break
-        if not ok:
-            return False
-    return True
+        if x - 1 >= 0 and m[x - 1][y] == m[x][y]:
+            last_cons[y] += 1
+        else:
+            last_cons[y] = 1
+        max_cons[y] = max(max_cons[y], last_cons[y])
+        if max_cons[y] >= K:
+            continue
+        else:
+            res = False
+    return res
 
 
-def go(i, case):
-    if i == len(case):
-        if check():
-            return True
-        return False
-
-    original_row = m[case[i]][:]
+def go(i, n, max_cons, last_cons):
+    global ans
+    if n >= ans:
+        return
+    max_cons, last_cons = max_cons[:], last_cons[:]
+    if i > 0 and check(i - 1, max_cons, last_cons):
+        ans = min(ans, n)
+    if i == len(m):
+        return
+        # 색을 안바꿀 경우
+    go(i + 1, n, max_cons, last_cons)
+    # 색을 바꿀 경우
+    original_row = m[i][:]
     for c in range(2):
-        m[case[i]] = [c] * W
-        if go(i + 1, case):
-            return True
-        m[case[i]] = original_row
-    return False
+        m[i] = [c] * W
+        go(i + 1, n + 1, max_cons, last_cons)
+        m[i] = original_row
 
 
 T = int(input())
@@ -42,14 +42,7 @@ for test_case in range(1, T + 1):
     D, W, K = map(int, input().split())
     m = [list(map(int, input().split())) for _ in range(D)]
     ok = False
-    max_cons = [0] * W
-    for ans in range(0, D):
-        for case in combinations(range(D), r=ans):
-            if go(0, case):
-                ok = True
-                break
-        if ok:
-            break
-    else:
-        ans = D
+
+    ans = D
+    go(0, 0, [1] * W, [1] * W)
     print(f'#{test_case} {ans}')
