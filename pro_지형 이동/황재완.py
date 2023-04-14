edbf772ef3e96002fe0b6b78e277a85faf24c5bb
Jaewan Hwang <jaewan9074@gmail.com>
2023-04-14 15:11:51 +0900

    2023.04.14

--- pro_지형 이동/황재완.py
+++ pro_지형 이동/황재완.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque, defaultdict
 import heapq
 
 m, group, N, H = None, 0, 0, 0
@@ -28,17 +28,15 @@ def solution(land, height):
         for y in range(N):
             if gm[x][y] == -1:
                 bfs(x, y, gm)
-    adj_mat = [[float('inf')] * group for _ in range(group)]
-    adj_list = [set() for _ in range(group)]
+    adj_list = [defaultdict(lambda: float('inf')) for _ in range(group)]
     for x in range(N):
         for y in range(N):
             for k in range(4):
                 nx, ny = x + dx[k], y + dy[k]
                 if 0 <= nx < N and 0 <= ny < N and gm[x][y] != gm[nx][ny]:
-                    adj_mat[gm[x][y]][gm[nx][ny]] = adj_mat[gm[nx][ny]][gm[x][y]] = min(adj_mat[gm[x][y]][gm[nx][ny]],
-                                                                                        abs(m[x][y] - m[nx][ny]))
-                    adj_list[gm[x][y]].add(gm[nx][ny])
-                    adj_list[gm[nx][ny]].add(gm[x][y])
+                    adj_list[gm[x][y]][gm[nx][ny]] = adj_list[gm[nx][ny]][gm[x][y]] = min(
+                        adj_list[gm[x][y]][gm[nx][ny]],
+                        abs(m[x][y] - m[nx][ny]))
 
     ans = 0
     visited = [False] * group
@@ -60,6 +58,9 @@ def solution(land, height):
             return ans
 
         for v in adj_list[u]:
-            if not visited[v] and min_edge[v] > adj_mat[u][v]:
-                heapq.heappush(q, (adj_mat[u][v], v))
+            if not visited[v] and min_edge[v] > adj_list[u][v]:
+                heapq.heappush(q, (adj_list[u][v], v))
     return ans
+
+
+print(solution([[1, 4, 8, 10], [5, 5, 5, 5], [10, 10, 10, 10], [10, 10, 10, 20]], 3))
